e171d3e58353450cd7d463b134b48b8f08cfc8a4
njugunamwangi <njugunamwangi.n@gmail.com>
2023-11-26 18:25:59 +0300

    Project test

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_a_user_can_create_a_project() {
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes);
+
+        $this->assertDatabaseHas('projects', $attributes);
+    }
+}
